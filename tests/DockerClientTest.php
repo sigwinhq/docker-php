f99a3633ca94092af52d8a6d79d91118d7e043b6
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2026-01-30 11:09:29 +0100

    Cleanup

--- tests/DockerClientTest.php
+++ tests/DockerClientTest.php
@@ -15,17 +15,74 @@ declare(strict_types=1);
 
 namespace Docker\Tests;
 
+use Docker\API\Model\ContainersCreatePostBody;
 use Docker\DockerClient;
+use Docker\DockerClientFactory;
+use Http\Client\HttpAsyncClient;
+use Psr\Http\Client\ClientInterface;
 
 /**
  * @internal
  */
 #[\PHPUnit\Framework\Attributes\CoversNothing]
-#[\PHPUnit\Framework\Attributes\Small]
+#[\PHPUnit\Framework\Attributes\Medium]
 final class DockerClientTest extends DockerTestCase
 {
     public function testCreate(): void
     {
         self::assertInstanceOf(DockerClient::class, DockerClient::create());
+    }
+
+    public function testHttpClientImplementsSyncInterface(): void
+    {
+        $httpClient = DockerClientFactory::create();
+        self::assertInstanceOf(ClientInterface::class, $httpClient);
+    }
+
+    public function testHttpClientImplementsAsyncInterface(): void
+    {
+        $httpClient = DockerClientFactory::create();
+        self::assertInstanceOf(HttpAsyncClient::class, $httpClient,
+            'HttplugClient should implement HttpAsyncClient for async support');
+    }
+
+    public function testContainerLifecycle(): void
+    {
+        $docker = self::getDockerClient();
+
+        // Pull image if needed
+        $docker->imageCreate('', [
+            'fromImage' => 'busybox:latest',
+        ]);
+
+        // Create container
+        $containerConfig = new ContainersCreatePostBody();
+        $containerConfig->setImage('busybox:latest');
+        $containerConfig->setCmd(['echo', '-n', 'output']);
+        $containerConfig->setAttachStdout(true);
+        $containerConfig->setLabels(new \ArrayObject(['docker-php-test' => 'true']));
+
+        $containerCreate = $docker->containerCreate($containerConfig);
+        self::assertNotNull($containerCreate->getId());
+
+        // Start container
+        $docker->containerStart($containerCreate->getId());
+
+        // Wait for container to finish
+        $docker->containerWait($containerCreate->getId());
+
+        // List containers to verify it exists
+        $containers = $docker->containerList(['all' => true]);
+        $found = false;
+        foreach ($containers as $container) {
+            if ($container->getId() === $containerCreate->getId()) {
+                $found = true;
+                break;
+            }
+        }
+        self::assertTrue($found, 'Container should be in the list');
+
+        // Cleanup
+        $docker->containerDelete($containerCreate->getId());
     }
 }
